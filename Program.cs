eef84dc093e839046caf6de30475278bb820899e
Emil <emil@Emils-MacBook-Pro.local>
2022-05-10 10:55:49 +0200

    Set up the basics of the program.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyBanker;
+
+internal class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("test");
+
+
+    }
+}
+
+internal abstract class Card {
+    Card() {
+
+    }
+}
